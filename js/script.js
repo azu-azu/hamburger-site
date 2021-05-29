d6bd44bbc1e1ba23059d3a4bfc7d97105f1b7229
azu-azu <azu-azu@users.noreply.github.com>
2021-05-29 20:39:08 +0900

    add new file

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// ハンバーガーボタン
+jQuery(function($) {
+  $(".js-menu-btn").on("click", function(){
+    $(this).toggleClass("is-open");
+    $(".l-sidebar").toggleClass("is-open");
+    $("body").toggleClass("is-open");
+    $(".p-has-menu").toggleClass("is-open");
+  });
+});
