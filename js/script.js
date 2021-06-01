709038ac87b3b306243d101ef1faa63742e91cfa
azu-azu <azu-azu@users.noreply.github.com>
2021-06-01 10:56:43 +0900

    1-layout

--- js/script.js
+++ js/script.js
@@ -1,19 +1,16 @@
 // Menuボタンを押したら
 jQuery(function($) {
   $(".js-menu-btn").on("click", function(){
-    $(this).toggleClass("is-open");
+    // $(this).toggleClass("is-open");
     $(".l-sidebar").toggleClass("is-open");
     $("body").toggleClass("is-open");
     $(".p-has-menu").toggleClass("is-open");
   });
-});
 
-$(function() {
-  $('js-sidex').on('click', function() {
-    if ( $(this).prop('checked') == false ) {
-
-    } else {
-      $('.l-sidebar').fadeOut();
-    }
+  $('.js-sidex-btn').on('click', function(){
+    // $(this).toggleClass("is-open");
+    $(".l-sidebar").toggleClass("is-open");
+    $("body").toggleClass("is-open");
+    $(".p-has-menu").toggleClass("is-open");
   });
 });
